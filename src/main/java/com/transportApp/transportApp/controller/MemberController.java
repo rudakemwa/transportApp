3ca193520a044bc74a25e1bdc08ae3b98e2a20eb
rudakemwa <rudakemwadanchancel@gmail.com>
2025-05-06 11:51:00 +0200

    memberContr

---
+++ src/main/java/com/transportApp/transportApp/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.transportApp.transportApp.controller;
+
+
+import com.transportApp.transportApp.model.Member;
+import com.transportApp.transportApp.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/members")
+
+public class MemberController {
+     @Autowired
+    private MemberService memberService;
+
+    @GetMapping
+    public List<Member> getAllMembers() {
+        return memberService.getAllMembers();
+    }
+
+    @GetMapping("/{id}")
+    public Member getMemberById(@PathVariable Long id) {
+        return memberService.getMemberById(id)
+                .orElseThrow(() -> new RuntimeException("Membre non trouvé"));
+    }
+
+    @PostMapping
+    public Member createMember(@RequestBody Member member) {
+        return memberService.createMember(member);
+    }
+
+    @PutMapping("/{id}")
+    public Member updateMember(@PathVariable Long id, @RequestBody Member member) {
+        return memberService.updateMember(id, member);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMember(@PathVariable Long id) {
+        memberService.deleteMember(id);
+    }
+}
